a77cc10075a5d15665a341dcb12a6c1315fc7b89
anuoluwapo <anuoluwapokudirat@yahoo.com>
2020-05-26 10:28:54 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavigationMobile />
+    <div class="content" :class="{'open':showNav}">
+      <div class="top-bar">
+        <div id="navigation-icon" v-if="mobileView"
+          @click="showNav = !showNav">
+          <i class="fas fa-bars"></i>
+          <div class="company-logo">
+            <img src="./assets/logo.png" alt="logo">
+          </div>
+        </div>
+        <Navigation v-if="!mobileView" />
+      </div>
+      <Content />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from "./components/Navigation.vue";
+import NavigationMobile from "./components/NavigationMobile.vue";
+import Content from "./components/Content.vue";
 
 export default {
-  name: 'App',
+  data: () => {
+    return {
+      mobileView: true,
+      showNav: false
+    };
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 1050;
+    }
+  },
   components: {
-    HelloWorld
+    Navigation,
+    NavigationMobile,
+    Content
+  },
+  created() {
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
   }
-}
+};
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url("https://use.fontawesome.com/releases/v5.9.0/css/all.css");
+* {
+  font-size: 1rem;
 }
+
+body {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  font-family: "Segoe UI", Tahoma;
+  background-color: #7ca971;
+}
+
+#app {
+  position: relative;
+  width: calc(100% - 20px);
+  height: calc(100vh - 20px);
+  padding: 10px;
+  color: #333;
+  overflow: hidden;
+}
+
+.top-bar {
+  display: flex;
+  width: 100%;
+}
+
+#navigation-icon {
+  padding: 10px 10px 20px;
+  margin-right: 10px;
+  cursor: pointer;
+
+  i {
+    font-size: 1.5rem;
+  }
+}
+
+.content {
+  position: absolute;
+  top: 0px;
+  left: 0;
+  width: 100%;
+  // width: calc(100% - 60px);
+  height: calc(100vh - 60px);
+  padding: 20px;
+  background-color: #fff;
+}
+
+.open {
+  transform: translateX(300px);
+}
+.company-logo{
+    margin-top: -46px;
+    margin-left: 120px;
+    height: 120px !important;
+    width: 40px !important;
+}
+
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="navigation">
+    <div class="logo">
+        <a href=""><img src="../assets/logo.png" alt=""></a>
+    </div>
+    <div class="search">
+      <input type="text" placeholder="Search..." />
+      <i class="fas fa-search"></i>
+    </div>
+    <ul>
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Store</li>
+      <li>Login</li>
+      <li class="sign-up-btn"><button>Sign Up</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#navigation {
+  display: flex;
+  width: 100%;
+  margin-bottom: 50px;
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-right: 30px;
+    margin-top: 10px;
+    li {
+      display: flex;
+      align-items: center;
+      font-size: 1.5rem;
+      padding: 2px 10px;
+      cursor: pointer;
+
+      &:hover {
+        color: #7ca971;
+      }
+    }
+    .sign-up-btn{
+        button{
+            padding: 5px 17px;
+            font-size: 18px;
+            background-color: #7ca971;
+            border: 1px solid #7ca971;
+            border-radius: 4px;
+            color: #fff;
+            cursor: pointer;
+            outline: none;
+
+            &:hover{
+                box-shadow: 0 3px 4px rgba(0,0,0,0.5);
+            }
+        }
+    }
+  }
+   .logo{
+       img{
+           height: 40px;
+           width: 120px;
+       }
+   }
+  .search {
+    position: relative;
+    width: 100%;
+    max-width: 400px;
+    margin: 0 auto;
+    input {
+      border: none;
+      outline: none;
+      width: calc(100% - 80px);
+      padding: 15px 60px 15px 20px;
+      margin: 0;
+      border-radius: 30px;
+      background-color: #efefef;
+      font-family: "Segoe UI", Tahoma;
+      font-size: 1rem;
+
+      &:hover{
+          box-shadow: 0 1px 3px 0 rgba(32,33,36,0.28);
+      }
+    }
+
+    i {
+      position: absolute;
+      right: 20px;
+      top: 15px;
+      font-size: 1.2rem;
+      color: #aaa;
+      cursor: pointer;
+    }
+  }
+}
+</style>
